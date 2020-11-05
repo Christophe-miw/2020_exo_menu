0dcf23c6fa851141b9f6d9b83e110385fae032b5
michelgege <barberot.b@gmail.com>
2020-11-05 16:04:07 +0100

    refactor css, only one toggleclass in js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 let button_part = document.querySelector('.menu_button_part')
-let menu = document.querySelector('.menu_container')
+let menu = document.querySelector('.menu')
 
 document.querySelector('.menu_button').addEventListener('click', () => {
-    button_part.classList.toggle('cross')
     menu.classList.toggle('open')
 })
