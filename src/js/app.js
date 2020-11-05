d6c67564b2645e62f860c407191912feae40d87e
michelgege <barberot.b@gmail.com>
2020-11-05 15:17:11 +0100

    inté faite , animation du burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-console.log('js fired')
+let button_part = document.querySelector('.menu_button_part')
+let menu = document.querySelector('.menu_container')
+
+document.querySelector('.menu_button').addEventListener('click', () => {
+    button_part.classList.toggle('cross')
+    menu.classList.toggle('open')
+})
